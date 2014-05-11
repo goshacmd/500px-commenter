b28eb1e566fe8c8717ed8bb233c9a7a1f2edd94f
Gosha Arinich <me@goshakkk.name>
2014-05-11 22:54:16 +0300

    refactor generator a bit

--- lib/comment_text_generator.rb
+++ lib/comment_text_generator.rb
@@ -1,4 +1,145 @@
 class CommentTextGenerator
+  class Token
+    class << self
+      attr_accessor :type
+
+      def lookup(type)
+        const_get type.to_s.capitalize
+      end
+
+      def build(type)
+        Token === type ? type : lookup(type).new
+      end
+    end
+
+    attr_reader :fill
+
+    def initialize(fill = nil)
+      @fill = fill
+    end
+
+    def filled?
+      !!fill
+    end
+
+    def fill_in(fill)
+      self.class.new(fill)
+    end
+
+    def transform(prefill)
+    end
+
+    def type
+      self.class.type
+    end
+
+    class Name < Token
+      self.type = :name
+    end
+
+    class Adjective < Token
+      self.type = :adjective
+    end
+
+    class Noun < Token
+      self.type = :noun
+    end
+
+    class Sign < Token
+      self.type = :sign
+    end
+
+    class Trash < Token
+      self.type = :trash
+
+      ARTICLE_PLACEHOLDER = ' (a)'
+
+      def transform(prefill)
+        if ends_in_article?
+          sub = article_sub(prefill)
+          fill.gsub!(ARTICLE_PLACEHOLDER, sub)
+        end
+      end
+
+      def ends_in_article?
+        !!fill[ARTICLE_PLACEHOLDER]
+      end
+
+      def article_sub(prefill)
+        if prefill.has?(:noun)
+          adj = prefill.find(:adjective).fill
+          noun = prefill.find(:noun).fill
+
+          plural = noun[-1] == 's'
+          vowel = %w(a e i o u y).include? adj[0]
+
+          plural ? '' : (vowel ? ' an' : ' a')
+        else
+          ''
+        end
+      end
+    end
+  end
+
+  class SentenceScheme
+    attr_reader :tokens
+
+    def initialize(tokens)
+      @tokens = tokens.map do |token|
+        Token.build(token)
+      end
+    end
+  end
+
+  class SentencePrefill
+    attr_reader :tokens
+
+    def initialize(tokens, fills)
+      @tokens = tokens.zip(fills).map do |raw_token, fill|
+        raw_token.fill_in(fill)
+      end
+    end
+
+    def transform
+      tokens.each { |token| token.transform(self) }
+
+      self
+    end
+
+    def has?(token_type)
+      tokens.map(&:type).include?(token_type)
+    end
+
+    def find(token_type)
+      tokens.find { |token| token.type == token_type }
+    end
+
+    def to_s
+      processed = []
+      buf = ""
+
+      tokens.each do |token|
+        type = token.type
+        value = token.fill
+
+        first = processed.empty?
+        value = value.capitalize if first
+        needs_space = !first && type != :sign
+
+        if type == :name
+          final = first ? "#{value}," : ", #{value}"
+        else
+          final = needs_space ? " #{value}" : value
+        end
+
+        processed << type
+        buf << final
+      end
+
+      buf
+    end
+  end
+
   class << self
     attr_accessor :data, :probabilities
 
@@ -28,14 +169,16 @@ class CommentTextGenerator
     end
 
     def random_text(photo)
-      rating = photo.rating
-      name = photo.user_firstname
+      if Hash === photo
+        rating, name = *photo.values_at(:rating, :name)
+      else
+        rating = photo.rating
+        name = photo.user_firstname
+      end
 
       scheme = random_scheme(name)
       prefill = prefill_scheme(scheme, rating: rating, name: name)
-      transformed = transform_prefill(prefill)
-      
-      join_prefill(transformed)
+      prefill.transform.to_s
     end
 
     def random_scheme(name = nil)
@@ -51,27 +194,29 @@ class CommentTextGenerator
 
       scheme << :sign if probable?(:sign)
 
-      scheme
+      SentenceScheme.new(scheme)
     end
 
     def prefill_scheme(scheme, rating:, name: nil)
-      scheme.map do |type|
-        val = type == :name ? name.strip.to_ascii : random_component(type, rating)
-        [type, val]
-      end
-    end
+      tokens = scheme.tokens
 
-    def transform_prefill(prefill)
-      prefill.map do |(type, value)|
-        [type, transform(type, value, prefill)]
+      fills = tokens.map do |token|
+        type = token.type
+
+        type == :name ? name.strip.to_ascii : random_component(type, rating)
       end
+
+      SentencePrefill.new tokens, fills
     end
 
     def join_prefill(prefill)
       processed = []
       buf = ""
 
-      prefill.each do |(type, value)|
+      prefill.tokens.each do |token|
+        type = token.type
+        value = token.fill
+
         first = processed.empty?
         value = value.capitalize if first
         needs_space = !first && type != :sign
@@ -87,32 +232,6 @@ class CommentTextGenerator
       end
 
       buf
-    end
-
-    def transform(type, value, prefill)
-      case type
-      when :trash
-        if !!value[' (a)']
-          has_noun = prefill.map(&:first).include?(:noun)
-
-          if has_noun
-            adj_first = prefill.find { |(t, _)| t == :adjective }[1][0]
-            noun_last = prefill.find { |(t, _)| t == :noun }[1][-1]
-
-            plural = noun_last == 's'
-            vowel = %w(a e i o u y).include? adj_first
-            rep = plural ? '' : (vowel ? ' an' : ' a')
-          else
-            rep = ''
-          end
-
-          value.gsub(' (a)', rep)
-        else
-          value
-        end
-      else
-        value
-      end
     end
 
     def random_component(type, rating)
